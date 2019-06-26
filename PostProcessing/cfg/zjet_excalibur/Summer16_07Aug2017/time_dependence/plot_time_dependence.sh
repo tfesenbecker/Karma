db778d3ea7695e40ed4a191b18ed80bc429c12f1
tfesenbecker <tabea.fessenbecker@student.kit.edu>
2019-06-26 14:42:55 +0200

    [PostProcessing] Bugs fixes for 2016: Keyerror in y_range, compatible with outputfiles with 'iov' in them

--- PostProcessing/cfg/zjet_excalibur/Summer16_07Aug2017/time_dependence/plot_time_dependence.sh
+++ PostProcessing/cfg/zjet_excalibur/Summer16_07Aug2017/time_dependence/plot_time_dependence.sh
@@ -21,7 +21,7 @@ palisade.py task zjet_excalibur plot_time_dependence \
     --sample 07Aug2017 \
     --corr-levels "L1L2L3" \
     --quantities {z,jet1,jet2,jet3}{pt,eta,phi} zmass met metphi mpf ptbalance rho npumean npv \
-    --run-periods Run2016{BCD,EFearly,FlateGH} \
+    --run-periods Run2016{B,C,D,E,F,G,H,BCDEFGH} \
     --time-quantity "run2016" \
     --channel "mm" "ee" \
     --output-dir "plots" \
